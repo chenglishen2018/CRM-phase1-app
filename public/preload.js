714167f0b1cb5968e6fccaa8048666735d5af1b3
chenglishen2018 <44375937+chenglishen2018@users.noreply.github.com>
2026-03-05 10:06:53 -0500

    Add files via upload

--- public/preload.js
+++ public/preload.js
@@ -1,1 +1,7 @@
+const { contextBridge } = require('electron');
 
+contextBridge.exposeInMainWorld('electron', {
+  appName: 'CRM Phase 1 Trial',
+  version: '1.0.0',
+  description: 'Phase I Dose-Escalation Clinical Trial Management'
+});
