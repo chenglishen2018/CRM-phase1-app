7838a9d02bc50741484743e021a6d3f5ca72e3bc
chenglishen2018 <44375937+chenglishen2018@users.noreply.github.com>
2026-03-05 10:07:59 -0500

    Add files via upload

--- shiny_app/run_app.R
+++ shiny_app/run_app.R
@@ -1,1 +1,15 @@
+# Install packages if needed
+packages <- c('shiny', 'shinydashboard', 'ggplot2', 'dplyr', 'DT', 'gridExtra', 'scales')
 
+for (pkg in packages) {
+  if (!require(pkg, character.only = TRUE)) {
+    install.packages(pkg, repos = 'http://cran.r-project.org')
+    library(pkg, character.only = TRUE)
+  }
+}
+
+# Run the Shiny app
+options(shiny.port = 3838)
+options(shiny.host = '127.0.0.1')
+
+shiny::runApp('CRM_App_Fresh.R', launch.browser = FALSE)
